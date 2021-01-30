424f84dbc089f448a7400292f78b903e44c7f074
Your Name <you@example.com>
2021-01-30 20:31:29 -0300

    It works nix build .#poetry2nixOCIImage

--- flake.nix
+++ flake.nix
@@ -4,19 +4,21 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+        pkgsAllowUnfree = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
+    in
+    {
+      packages.poetry2nixOCIImage = import ./poetry2nixOCIImage.nix {
+        pkgs = nixpkgs.legacyPackages.${system};
+      };
 
-    packages.myExampleFlake = import ./default.nix {
-      pkgs = nixpkgs.legacyPackages.${system};
-    };
+    #defaultPackage = self.packages.${system}.myExampleFlake;
 
-    packages.myExampleFlake2 = import ./default2.nix {
-      pkgs = nixpkgs.legacyPackages.${system};
-    };
-
-    defaultPackage = self.packages.${system}.myExampleFlake;
-
-    packages.${system} = self.packages.myExampleFlake2;
+    #packages.${system} = self.packages.myExampleFlake2;
   });
 
 }
