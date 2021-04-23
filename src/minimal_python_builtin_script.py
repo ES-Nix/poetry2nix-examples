8bbbc154c601e2ca84932499ceda44e447cb0650
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-04-23 01:22:31 -0300

    Includes src folder and a refactor that creates utils.py, it works

---
+++ src/minimal_python_builtin_script.py
@@ -0,0 +1,9 @@
+from utils import cubic_function
+
+
+def main():
+    print(list(map(cubic_function, range(20))))
+
+
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,6 @@
-
 def cubic_function(x, a=1, b=0, c=0, d=0):
     """
     https://en.wikipedia.org/wiki/Cubic_function
     :return: the result of a cubic function
     """
     return a*x**3 + b*x**2 + c*x + d
-
-
-def main():
-    print(list(map(cubic_function, range(20))))
-
-
-if __name__ == '__main__':
-    main()
