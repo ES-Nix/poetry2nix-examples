232dc036a695deb17a6ee6ff817a678a3d0528d7
Pedro <pedroalencarregis@hotmail.com>
2023-04-26 00:30:02 -0300

    Tests: fixed pandas = "1.2.2"

--- flake.nix
+++ flake.nix
@@ -99,7 +99,7 @@
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
             curl
-            poetryEnv
+            #poetryEnv
             # podman-rootless.defaultPackage.${system}
             poetry
             ripgrep
@@ -112,7 +112,7 @@
             export TMPDIR=/tmp
             echo "Entering the nix devShell"
 
-            echo "${poetryEnv}/${poetryEnv.sitePackages}"
+            echo
 
             # ln -fv "''${poetryEnv}/''${poetryEnv.sitePackages}" .venv
           '';
