7da1c8e8576a921f809120be2674afd4875c22b9
Pedro <pedroalencarregis@hotmail.com>
2023-04-26 00:21:38 -0300

    Tests: pd.__version__

--- flask_minimal_example.py
+++ flask_minimal_example.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world!!'
+
+@app.route('/pandas')
+def index():
+    return pd.__version__
 
 def main():
     app.run(host='0.0.0.0')
